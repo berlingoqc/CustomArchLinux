a605fee41d15732dab01b4f73467d242bee165fb
berlingoqc <william95quintalwilliam@outlook.com>
2016-12-05 12:04:39 -0500

    Must to iso repacking

--- isoConfig/archisotool.sh
+++ isoConfig/archisotool.sh
@@ -19,9 +19,10 @@
 
 MIRROR="http://mirror.rackspace.com/archlinux/iso/"
 RELEASE=$(date +%Y.%m.01)
-DIRISO="${PWD}/customiso/"
-FILE="archlinux-${RELEASE}-dual.iso"
-
+WORKINGDIR="${PWD}"
+DIRISO="${WORKINGDIR}/customiso"
+FILE="${WORKINGDIR}/archlinux-${RELEASE}-dual.iso"
+ARCH="x86_64"
 E_CS=1
 E_NOARGS=3
 
@@ -33,13 +34,16 @@ usage: ${0##*/} [-d [mirror] | -f <file>] [...]
 	-d [mirror]	Download the lastest iso from https server
        			default is $MIRROR	
 	-f <file>	Use specify archlinux iso
-        -a [arch]	Specify witch architecture to use
+    	-a [arch]	Specify witch architecture to use
 			default is $ARCH
 	-l <dir>	Specify a directory to use for the other
 			manipulation
 			
 	-e 		Extract iso to a default is ~/customiso
-	
+	-s [folder]	Add default installation strap, if folder
+			copy the contain to the strap location must
+			have an install.sh file to start the strap	
+
 	-i <file>	Read from file package from pacman or AUR
        			and install them in the iso	
 	-s <folder>	Copy folder and execute script in the iso root
@@ -55,22 +59,46 @@ usage: ${0##*/} [-d [mirror] | -f <file>] [...]
 EOF
 }
 
-[ $# -eq 0 ] && { usage; exit $E_NOARGS; }
-
-. funcIso.sh
-
-while getopts "df:al:ei:s:m:r" o; do
- case ${o} in
+[ $# -eq 0 ] && { echo "Argument requireds !"; usage; exit $E_NOARGS; }
+source ./funcIso.sh
+fetch_req
+while (( $# > 0 )) ; do
+ case "$1" in
 	-d)
-	
+	  if [[ $2 != "-*" && $2 != "" ]]; then
+	    MIRROR=$1
+		shift 1
+	    #Could valid mirror
+      fi
+  	  download_iso
 	;;
 	-f)
-	:
+	  if [[ $2 != "-*" && $2 != "" ]]; then
+	  	if [[ -f $2 ]]; then
+		  echo "Use this iso for later operation $2"
+		  FILE=$2
+		else
+		  echo "File do not exists ! Please try again"
+		  exit 1
+		fi
+	  fi
+	;;
+	-e)
+		#If iso is present on working dir do the magic
+		copy_iso 
 	;;
 	-s)
-	:
+	  if [[ $2 != "-*" && $2 != "" ]]; then
+	    if [[ -d $2 && -f $2/install.sh ]]; then
+	      arg=$2
+	      shift 1
+	    fi
+	  fi
+	  add_install_strap $arg
 	;;	
+	-h)
+	  usage ; exit 0
+	;;
  esac
+ shift 1
 done
-
-
--- isoConfig/funcIso.sh
+++ isoConfig/funcIso.sh
@@ -19,42 +19,53 @@
 #       CREATED: 12/02/2016 10:07:38 AM
 #      REVISION:  ---
 #===============================================================================
-
+su_copy ()
+{
+  mkdir -p "/mnt/archiso"
+  mount -t iso9660 "$FILE" "/mnt/archiso" && \
+  echo "Copying files from iso in the ${DIRISO} it would not be long" && \ 
+  cp -a /mnt/archiso ${DIRISO} && \
+  umount ${FILE} && \
+  rmdir /mnt/archiso
+} #Copy the contain of the iso to a specific directory
 #Execute the cmd past as argument with root privileges
 with_su () 
 {
- [ $UID -ne 0 && ! -n "$1" ] && \ 
- { echo "Must be root for this part : "; su -c "$1"; } || $1
+ if [ $UID -ne 0 ]; then 
+   echo "Must be root for this part"
+   su "$1"
+ else
+   $1
+ fi
 }
 
 #Valid if a accepted architecture have been past
 valid_arch () 
 {
-  ARC=x86_64
-  if [ -e "$1" && (( "$1" == "i386" || "$1" == "x86_64" ))  ]; then
+  ARC="x86_64"
+  if [[ "$1" == "i386" || "$1" == "x86_64"  ]]; then
     ARC=$1
-  else
-    return 0
   fi
   return $ARC
 }
 
 #Fetch required script to run programs
 #and install missing dependecies
-fetch_required ()
+fetch_req ()
 {
   #Fonction to download required script 
   #arch-chroot
-  wget https://git.archlinux.org/arch-install-scripts.git/plain/arch-chroot.in
+  if [ ! -f "arch-chroot.in" ]; then
+    wget https://git.archlinux.org/arch-install-scripts.git/plain/arch-chroot.in
+  fi
 }
 
 #Unpack the contain of the airootfs to allow chroot into it
 unsquash ()
 {
-  ARC=valid_arch $1 
-  echo "Unsquashing the $ARC system"
-  PAT="${DIRISO}/arch/${ARC}/airootfs.sfs"
-  unsquashfs $PAT
+  echo "Unsquashing the $ARCH system"
+  PAT="${DIRISO}/arch/${ARCH}/airootfs.sfs"
+  unsquashfs $PAT 
 }
 
 #Execut the argument in the root fs on the iso working on
@@ -63,11 +74,11 @@ arch_chroot ()
   #Pass as argument arch to use  and witch local files to copy
   #and run in the chroot environnement
   local ARC PRE FILES
-  ARC=valid_arch $1
-  if [ $ARC == "i386" ]; then
+  ARCH=valid_arch $1
+  if [ $ARCH == "i386" ]; then
     PRE="setarch i686 "
   fi
-  PATDIR=${DIRISO}/arch/$ARC/squashfs-root
+  PATDIR=${DIRISO}/arch/$ARCH/squashfs-root
   mkdir ${PATDIR}/configScript
   # Test if all script pass exists and create a file to call them alls
   FILES="#!/bin/bash\n\n"
@@ -91,7 +102,8 @@ arch_chroot ()
 download_iso ()
 {
   echo "Downloading latest arch iso from $MIRROR"
-  wget ${MIRROR}/${RELEASE}{${FILE},md5sums.txt}
+  wget ${MIRROR}${RELEASE}/{${FILE},md5sums.txt}
+  [[ $1 -ne 0 ]] && { cat $?; exit 1; }
   md5sum -c md5sums.txt &> /dev/null
   if [ $? -ne 0 ]; then
     echo "Error with checksum , try again"
@@ -99,24 +111,15 @@ download_iso ()
   fi
 }
 
-#Copy the contain of the iso to a specific directory
 copy_iso ()	
 {
-  [ -d $DIRISO ] && . rm -r $DIRISO
+  [ -d $DIRISO ] && rm -r $DIRISO
   if [ ! -f ${FILE} ]; then
     download_iso
-  fi
-  #Mount the iso and copie the files
-  cmd={
-   mkdir -p /mnt/archiso
-   mount -t iso9660 ${PWD}/$FILE /mnt/archiso
-   echo "Copying files from iso in the ./customiso/ it won't be long"
-   cp -a /mnt/archiso $DIRISO
-   umount ${PWD}/$FILE
-   rmdir /mnt/archiso
-}
-  #If not root run this command throw su and ask for passwd
-  with_su $cmd
+  fi #If not root run this command throw su and ask for passwd
+  with_su su_copy
+  unsquash
+
 }
 
 #Repack the airootfs after the modification where made
@@ -130,3 +133,66 @@ squash_back ()
   md5sum airootfs.sfs > airootfs.md5
 }
 #Missing part for repacking the iso for dvd or usbkey or UEFI system
+iso_repack ()
+{
+  #Valid if fs-root have been squash back and delete
+  #if not error
+  _iso_efi=""
+  if [[ -f ${DIRISO}/EFI/archiso/efiboot.img ]]; then
+  	_iso_efi="-eltorito-alt-boot
+  	    	  -e ${DIRISO}/EFI/archiso/efiboot.img
+    	    	  -no-emul-boot -isohybris-gpt-basdat"
+  fi
+  ISO_LABEL=$(date ARCH_+%Y%m)
+  DRE=${DIRISO}/isolinux
+  if [[ ! -d ${WORKINGDIR}/squashfs-root ]]; then
+    if [[ $1 == "memstick" ]]; then
+	echo "Create isomemstick named $(basename $2)"
+	xorriso -as mkisofs \
+	      -iso-level 3 \
+	      -full-iso9660-filenames \
+	      -volid $ISO_LABEL \
+	      -eltorito-boot ${DRE}/isolinux.bin \
+	      -eltorito-catalog ${DRE}/boot.cat \
+	      -no-emul-boot -boot-load-size 4 -boot-info-table \
+	      -isohybrid-mbr ${DRE}/isohdpfx.bin \
+	      ${_iso_efi} \
+	      -output $2 \
+	      ${DIRISO}
+    elif [[ $1 == "disk" ]]; then
+      echo "Create iso for DVD/CD/BD name $(basename $2)"
+      genisoimage -l -r -J -V $ISO_LABEL -b ${DRE}/isolinux.bin \
+	      -no-emul-boot -boot-load-size 4 -boot-info-table -c \
+	      ${DRE}/boot.cat -o $2 ${DIRISO}
+    fi
+  fi    
+}
+
+#add an installation bootstrap in the airootfs
+add_install_strap ()
+{
+  #Create a directory and create the base script who gonna get execute at boot
+  #by adding it to the .zlogin
+  WKD=${WORKINGDIR}/squashfs-root/root
+  if [[ -d $WKD ]]; then
+    WKD=${WKD}/inststrap
+    if [[ -d $WKD ]]; then
+	echo "instrap folder already create do you want to replace (y/n)"
+	read yn
+        echo	
+	if [[ $yn == "y" ]]; then
+	  echo "Erasing folder"; rm -r $WKD 
+	else
+	  return
+	fi
+    fi
+    mkdir -p ${WKD}
+    if [[ $1 == "" ]]; then
+      echo -e "#!/bin/bash\necho Default installation strap" > ${WKD}/install.sh
+    else
+      cp -R $1/* $WKD
+    fi
+    echo "~/instastrap/install.sh" >> ${WKD}/../.zlogin
+  fi
+}
+
